448717f976da8f5caa16e8b1ed504192961d9d13
AyishaGisel <why_emailme@yahoo.com>
2018-07-06 18:15:41 -0400

    we got error free homepage rendering!

---
+++ app/meal.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Meal = /** @class */ (function () {
+    function Meal() {
+    }
+    return Meal;
+}());
+exports.Meal = Meal;
+//# sourceMappingURL=meal.js.map
---
+++ app/meal.ts
@@ -0,0 +1,7 @@
+export class Meal {
+  name: string; // BLDS
+  date: string; // YYYYMMDD
+  location: number; // restaurant ID
+  amount: string;
+}
+
